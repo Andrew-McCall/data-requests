84573d60490aa508403aefc081afb8f08a2e0618
Work <andrewdavidmccall@hotmail.com>
2023-11-30 11:34:08 +0000

    Updated to  be familiar

--- src/components/KingsDataLoad.jsx
+++ src/components/KingsDataLoad.jsx
@@ -5,16 +5,22 @@ function KingsDataLoad(){
 
     const [kings, setKings] = useState([]); 
 
+    const kingsData = []
+
+    for (let king of kings){
+        kingsData.push(<p>{king.nm}</p>)
+    }
+
     function getData(){
         axios.get("https://raw.githubusercontent.com/JHarry444/LBG-React-Demo/main/src/data/kings.json")
-            .then((response) => {setKings(response.data)})
+            .then((response) => { setKings(response.data)} )
             .catch((error) => {console.log(error)})
     }
 
     return (
     <div>
         <h1> Data </h1>
-        {kings.map((king) => <p>{king.nm}</p>)}
+        {kingsData}
         <button onClick={() => getData()} > Get Data </button>
     </div>)
 }
