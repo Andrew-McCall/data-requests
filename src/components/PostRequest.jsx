9772f3d6412acbedebecc4e6a91686da399b8ad9
Work <andrewdavidmccall@hotmail.com>
2023-12-01 10:10:22 +0000

    Choose your own name

--- src/components/PostRequest.jsx
+++ src/components/PostRequest.jsx
@@ -3,15 +3,13 @@ import { useEffect, useState } from "react";
 
 function PostRequest(){
     // Each use state for local variables which will require the page to rerender
-    const [user, setUser] = useState("No User Yet")
+    const [user, setUser] = useState({userName: "", isValid:false})
     const [message, setMessage] = useState([])
     const [ourMessage, setOurMessage] = useState("")
 
     // Upon page load, post for a new user and save to user useState
     useEffect(() => {
-        axios.post("http://165.120.188.147:2514/createAccount").then((res) => {
-            setUser(res.data.user)
-        }).catch(console.log)
+        getMessages();
     }, [])
 
     // Function to get all messages
@@ -28,19 +26,31 @@ function PostRequest(){
 
     // Send a message. Axios post, with a data object
     function sendMessage(){
-        axios.post("http://165.120.188.147:2514/sendMessage", {user:user, message:ourMessage}).then( () => {
+        axios.post("http://165.120.188.147:2514/sendMessage", {user:user.userName, message:ourMessage}).then( () => {
             getMessages()
         }).catch(console.log)
+        setOurMessage("")
     }
 
+    function login(){
+        axios.post("http://165.120.188.147:2514/createAccount", {user:user.userName})
+            .then( (res) => {
+                setUser({userName:user.userName, isValid:true})
+            }).catch(console.log)
+    }
 
     // Final html for page. user header, refresh button to load messages, input value for new messages, send button to send input data, html messages to view older messages
     return(<div>
 
-        <h1> My message board: {user} </h1>
-        <button onClick={() => getMessages()}> Refresh </button><br/>
-        <input value={ourMessage} onChange={(e) => setOurMessage(e.target.value)}></input>
-        <button onClick={() => sendMessage()}> SEND </button>
+        <h1> My message board:
+            <input disabled={user.isValid} value={user.userName} onChange={(e) => setUser({userName:e.target.value})}></input>
+            { !user.isValid && <button onClick={() => login()}> Login </button>}
+        </h1>
+        <div hidden={!user.isValid}>
+            <button onClick={() => getMessages()}> Refresh </button><br/>
+            <input value={ourMessage} onChange={(e) => setOurMessage(e.target.value)}></input>
+            <button onClick={() => sendMessage()}> SEND </button>
+        </div>
         {htmlMessages}
 
     </div>)
